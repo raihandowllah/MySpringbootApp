9730fb86a7c9003887888bfe21671ce0d32e4a83
raihandowllah <raihan0411@gmail.com>
2024-08-07 16:47:28 +0530

    Initial commit

---
+++ src/main/java/com/raihan/MySpringbootApp/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.raihan.MySpringbootApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
